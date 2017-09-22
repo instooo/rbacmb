c117b09b59eb5ecc61602f21cac1075581128f5f
dxl <710190573@qq.com>
2017-09-22 13:55:03 +0800

    部分文件修改

--- Application/Home/Controller/ProductController.class.php
+++ Application/Home/Controller/ProductController.class.php
@@ -11,6 +11,7 @@ class ProductController extends CommonController {
 		//查找关键字和描述
 		$webinfo = M('webconfig')->where('id=1')->find();
 	}
+	//首页
     public function index(){
 		$typeid = $_GET['id'];
 		$typeid = $typeid?$typeid:33;
@@ -26,4 +27,51 @@ class ProductController extends CommonController {
 		$this->assign ( 'typeid', $typeid );
         $this->display();
     }
+	//城市更新
+	public function update(){
+		$typeid =38;
+		$des = $this->get_cate_info($typeid);
+		
+		$typeid =29;
+		$list = $this->get_list($typeid);	
+		foreach($list as $key=>$val){
+			$listnew[$key]=$val;
+			$listnew[$key]['imgarr'] = explode("|",trim($val['img_duo'],"|"));
+		}		
+		$this->assign('list',$listnew);		
+		$this->assign('des',$des);	
+		$this->display();
+	}
+	//置业投资
+	public function invest(){
+		$typeid =39;
+		$des = $this->get_cate_info($typeid);
+		
+		$typeid =19;
+		$list = $this->get_list($typeid);	
+		foreach($list as $key=>$val){
+			$listnew[$key]=$val;
+			$listnew[$key]['imgarr'] = explode("|",trim($val['img_duo'],"|"));
+		}		
+		$this->assign('list',$listnew);		
+		$this->assign('des',$des);	
+		$this->display();
+	}
+	//置业投资
+	public function service(){
+		$typeid =20;
+		$des = $this->get_cate_info($typeid);
+		
+		$typeid =40;
+		$list = $this->get_list($typeid);			
+		$this->assign('toplist',$list);
+
+		$typeid =41;
+		$list = $this->get_list($typeid);			
+		$this->assign('botlist',$list);
+		
+		$this->assign('des',$des);	
+		$this->display();
+	}
+	
 }
